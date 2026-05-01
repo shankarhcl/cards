517b2a47225e5550842d2b0ab3469b84b92e4faf
shankarhcl <79025091+shankarhcl@users.noreply.github.com>
2026-05-01 22:25:31 +0530

    controller and swagger config

--- src/main/java/com/microservice/cards/CardsApplication.java
+++ src/main/java/com/microservice/cards/CardsApplication.java
@@ -1,5 +1,10 @@
 package com.microservice.cards;
 
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
@@ -13,6 +18,26 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 // To enable auditing in our application, we use the @EnableJpaAuditing annotation. This annotation is typically placed on the main application class (the class annotated with @SpringBootApplication) to enable JPA auditing features throughout the application. By specifying auditorAwareRef = "auditAwareImpl",
 @EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Cards microservice REST API Documentation",
+				description = "Cards microservice REST API Documentation",
+				version = "v1",
+				contact = @Contact(
+						name = "Shankar Sharma",
+						email = "shankar.sharma89202@gmail.com",
+						url = "https://github.com/shankarhcl"
+				),
+				license = @License(
+						name = "Apache 2.0",
+						url = "https://www.apache.org/licenses/LICENSE-2.0"
+				)
+		),
+		externalDocs = @ExternalDocumentation(
+				description = "Loans MS External Documentation",
+				url = "https://github.com/shankarhcl/cards"
+		)
+)
 public class CardsApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/microservice/cards/controller/CardsController.java
+++ src/main/java/com/microservice/cards/controller/CardsController.java
@@ -1,4 +1,155 @@
 package com.microservice.cards.controller;
 
+import com.microservice.cards.dto.CardsDto;
+import com.microservice.cards.dto.ErrorResponseDto;
+import com.microservice.cards.dto.ResponseDto;
+import com.microservice.cards.service.ICardsService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Pattern;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import static com.microservice.cards.constants.CardsConstants.*;
+import static org.springframework.http.HttpStatus.*;
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+
+@Tag(
+        name = "CRUD REST APIs for Cards MS",
+        description = "CRUD REST APIs in Cards MS to CREATE, UPDATE, FETCH AND DELETE card details"
+)
+@RestController
+@RequestMapping(path = "${api.version}/cards", produces = APPLICATION_JSON_VALUE)
+@AllArgsConstructor // this will generate constructor for us for ILoansService
+@Validated // for validations
 public class CardsController {
+
+    private ICardsService iCardsService;
+
+    @Operation(
+            summary = "Create Card REST API",
+            description = "REST API to create new Card"
+    )
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "201",
+                    description = "HTTP Status CREATED"
+            ),
+            @ApiResponse(
+                    responseCode = "500",
+                    description = "HTTP Status Internal Server Error",
+                    content = @Content(
+                            schema = @Schema(implementation = ErrorResponseDto.class))
+            )}
+    )
+    @PostMapping("/create")
+    public ResponseEntity<ResponseDto> createCard(@Valid @RequestParam
+                                                  @Pattern(regexp="(^$|\\d{10})",message = "Mobile number must be 10 digits")
+                                                  String mobileNumber) {
+        iCardsService.createCard(mobileNumber);
+        return ResponseEntity
+                .status(CREATED)
+                .body(new ResponseDto(STATUS_201, MESSAGE_201));
+    }
+
+    @Operation(
+            summary = "Fetch Card Details REST API",
+            description = "REST API to fetch card details based on a mobile number"
+    )
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "HTTP Status OK"
+            ),
+            @ApiResponse(
+                    responseCode = "500",
+                    description = "HTTP Status Internal Server Error",
+                    content = @Content(
+                            schema = @Schema(implementation = ErrorResponseDto.class))
+            )}
+    )
+    @GetMapping(value = "/fetch")
+    public ResponseEntity<CardsDto> fetchCardDetails(@RequestParam
+                                                     @Pattern(regexp="(^$|\\d{10})",message = "Mobile number must be 10 digits")
+                                                     String mobileNumber) {
+        CardsDto cardsDto = iCardsService.fetchCard(mobileNumber);
+        return ResponseEntity.status(OK).body(cardsDto);
+    }
+
+    @Operation(
+            summary = "Update Card Details REST API",
+            description = "REST API to update card details based on a card number"
+    )
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "HTTP Status OK"
+            ),
+            @ApiResponse(
+                    responseCode = "417",
+                    description = "Expectation Failed"
+            ),
+            @ApiResponse(
+                    responseCode = "500",
+                    description = "HTTP Status Internal Server Error",
+                    content = @Content(
+                            schema = @Schema(implementation = ErrorResponseDto.class))
+            )}
+    )
+    @PutMapping("/update")
+    public ResponseEntity<ResponseDto> updateCardDetails(@Valid @RequestBody CardsDto cardsDto) {
+        boolean isUpdated = iCardsService.updateCard(cardsDto);
+        if(isUpdated) {
+            return ResponseEntity
+                    .status(OK)
+                    .body(new ResponseDto(STATUS_200, MESSAGE_200));
+        }else{
+            return ResponseEntity
+                    .status(EXPECTATION_FAILED)
+                    .body(new ResponseDto(STATUS_417, MESSAGE_417_UPDATE));
+        }
+    }
+
+    @Operation(
+            summary = "Delete Card Details REST API",
+            description = "REST API to delete Card details based on a mobile number"
+    )
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "HTTP Status OK"
+            ),
+            @ApiResponse(
+                    responseCode = "417",
+                    description = "Expectation Failed"
+            ),
+            @ApiResponse(
+                    responseCode = "500",
+                    description = "HTTP Status Internal Server Error",
+                    content = @Content(
+                            schema = @Schema(implementation = ErrorResponseDto.class))
+            )}
+    )
+    @DeleteMapping("/delete")
+    public ResponseEntity<ResponseDto> deleteCardDetails(@RequestParam
+                                                         @Pattern(regexp="(^$|[0-9]{10})",message = "Mobile number must be 10 digits")
+                                                         String mobileNumber) {
+        boolean isDeleted = iCardsService.deleteCard(mobileNumber);
+        if(isDeleted) {
+            return ResponseEntity
+                    .status(OK)
+                    .body(new ResponseDto(STATUS_200, MESSAGE_200));
+        }else{
+            return ResponseEntity
+                    .status(EXPECTATION_FAILED)
+                    .body(new ResponseDto(STATUS_417, MESSAGE_417_DELETE));
+        }
+    }
 }
